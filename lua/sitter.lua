3e4e82210ed21ff945bba89c5adbd728e9a7832f
saurabh wajage <saurabhwajage887@gmail.com>
2023-12-30 15:39:20 +0530

    updated names

--- lua/sitter.lua
+++ lua/sitter.lua
@@ -1,21 +1,18 @@
+require 'nvim-treesitter.install'.prefer_git = false
+require 'nvim-treesitter.install'.compilers = { "clang" }
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = {"lua","typescript" ,"javascript","html" ,"css"},
+  ensure_installed = {"lua"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "" },
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+  auto_install = false,
 
   highlight = {
     enable = true,
+
+   additional_vim_regex_highlighting = false,
   },
 }
